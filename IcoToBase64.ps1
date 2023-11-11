9400c8ccdbd3718e2f6300e0dcbee7608d07c13d
skimballmis <131704647+skimballmis@users.noreply.github.com>
2023-11-10 18:36:02 -0600

    IconToBase64Encoder
    Just very useful when you want to inject an icon image as a part of a script especially helpful with desktop icon creation.

---
+++ IcoToBase64.ps1
@@ -0,0 +1,5 @@
+﻿#just having fun with ways to deploy a desktop shortcut or... other things...
+$IconPath = 'C:\Path\To\someIcon.ps1'
+$IconBytes = Get-Content $IconPath -Encoding Byte
+$IconBase64 = [System.Convert]::ToBase64String($IconBytes)
+$IconBase64
